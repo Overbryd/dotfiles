c6906c5bd5306a32c9ce1c6e5592584810435312
Lukas Rieder <l.rieder@gmail.com>
2014-12-09 21:41:17 +0100

    decrypt should obviously decrypt files, not encrypt them :D

--- decrypt.sh
+++ decrypt.sh
@@ -3,6 +3,6 @@
 for file in `find . -name '*.enc'`; do
   target=${file%%.enc}
   echo "Decrypting '$file' to '$target'..."
-  openssl aes-256-cbc -in "$file" -out "$target"
+  openssl enc -d -aes-256-cbc -in "$file" -out "$target"
 done
 
