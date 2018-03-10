00febf20ecbdf8e0a83980694a797b5b508d7c84
Lukas Rieder <l.rieder@gmail.com>
2018-03-10 14:48:41 +0100

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# generate a new, strong rsa ssh key
+ssh-keygen -t rsa -b 4096
+
+# wait for the user to add it to github
+pbcopy < ~/.ssh/id_rsa.pub
+echo "Now login to https://github.com/settings/keys and add the key that has already been copied to your clipboard."
+echo "Press any key to continue. Ctrl-C to abort."
+read
+
+# install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v
+
+# clone my dotfiles and make them
+git clone git@github.com:Overbryd/dotfiles.git ~/dotfiles
+make -C ~/dotfiles
