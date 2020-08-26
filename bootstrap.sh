64370763dc7fc2602ea875e7c498e800e642a1c2
Lukas Rieder <l.rieder@gmail.com>
2020-08-26 21:04:05 +0200

    Cleanup some more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# generate a new, strong rsa ssh key
+# Generate a new, strong rsa ssh key
 ssh-keygen -t rsa -b 4096
 
-# wait for the user to add it to github
+# Wait for the user to add it to github
 pbcopy < ~/.ssh/id_rsa.pub
 echo "Now login to https://github.com/settings/keys and add the key that has already been copied to your clipboard."
 read -p "Press any key to continue. Ctrl-C to abort."
 
-# install Xcode Command Line Tools
+# Install Xcode Command Line Tools
 # https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 PROD=$(softwareupdate -l |
@@ -18,6 +18,6 @@ PROD=$(softwareupdate -l |
   tr -d '\n')
 softwareupdate -i "$PROD" -v
 
-# clone my dotfiles and make them
+# Clone my dotfiles and make them
 git clone git@github.com:Overbryd/dotfiles.git ~/dotfiles
 make -C ~/dotfiles bootstrap
