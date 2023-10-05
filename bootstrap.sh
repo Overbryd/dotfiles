624119747ad1496e3ff1d40dff5f0ef2e548a34a
Lukas Rieder <l.rieder@gmail.com>
2023-10-05 12:16:22 +0200

    new bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-# Generate a new, strong rsa ssh key
+# Set a hostname for the computer
+hostname="$(scutil --get ComputerName)"
+echo "Current hostname: $(scutil --get LocalHostName) and computer name: $hostname"
+read -p "Enter a hostname (default: $hostname): " -r hostname
+if [[ "x" = "x$hostname" ]]; then
+  hostname="$(scutil --get ComputerName)"
+fi
+sudo scutil --set HostName "$hostname"
+sudo scutil --set LocalHostName "$hostname"
+sudo scutil --set ComputerName "$hostname"
+
+exit
+
+# Generate a new ssh key
 if ! test -f ~/.ssh/id_rsa; then
-  ssh-keygen -t rsa -b 4096
+  ssh-keygen -t ed25519
 
   # Wait for the user to add it to github
   pbcopy < ~/.ssh/id_rsa.pub
@@ -13,13 +26,26 @@ fi
 # Install Xcode Command Line Tools
 # https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" -v
+COMMAND_LINE_TOOLS=$(
+  softwareupdate --list \
+  | grep "\*.*Command Line" \
+  | head -n 1 | awk -F"*" '{print $2}' \
+  | sed -e 's/^ *//' \
+  | tr -d '\n'
+)
+softwareupdate --install "$COMMAND_LINE_TOOLS" --verbose
 
 # Clone my dotfiles and make them
-git clone git@github.com:Overbryd/dotfiles.git /usr/local/dotfiles
-make -C /usr/local/dotfiles bootstrap-administrator
+if ! test -d /usr/local/dotfiles; then
+  git clone --bare git@github.com:Overbryd/dotfiles.git --git-dir=/usr/local/dotfiles --work-tree="$HOME"
+  git --git-dir=/usr/local/dotfiles config --local status.showUntrackedFiles no
+  git --git-dir=/usr/local/dotfiles --work-tree="$HOME" checkout --force
+fi
+
+# Make this user
+if id -Gn $(id -un) | grep -qw admin; then
+  make -C /usr/local/dotfiles bootstrap-administrator
+else
+  make -C /usr/local/dotfiles bootstrap
+fi
+
