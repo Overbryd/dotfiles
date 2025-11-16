ea8d0425f12db1d99dcdd31fd0d22adfa0c57a3b
Lukas Rieder <l.rieder@gmail.com>
2025-11-16 16:49:32 +0100

    Set up root markers globally and comment out individual LSP server configurations
    - Added global LSP configuration for root_markers set to '.git'
    - Commented out individual LSP server setups for yamlls, jsonls, terraform-ls, elixirls, tailwindcss, and html
    - Retained cmp_capabilities import and log level setting for LSP debugging control

--- .config/nvim/lspconfig.lua
+++ .config/nvim/lspconfig.lua
@@ -1,87 +1,90 @@
-local lsp = require('lspconfig')
+vim.lsp.config('*', {
+  root_markers = { '.git' },
+})
+
 local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- tame lsp logging when not debugging
 vim.lsp.set_log_level("error")
 
 -- yamlls
-lsp.yamlls.setup{
-  capabilities=cmp_capabilities,
-  on_attach=on_attach,
-  -- capabilities=require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  settings={
-    redhat={
-      telemetry={
-        enabled=false
-      }
-    },
-    yaml={
-      schemas={
-        ["https://json.schemastore.org/github-workflow.json"]="/.github/workflows/*"
-      }
-    }
-  }
-}
+-- vim.lsp.yamlls.setup{
+--   capabilities=cmp_capabilities,
+--   on_attach=on_attach,
+--   -- capabilities=require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+--   settings={
+--     redhat={
+--       telemetry={
+--         enabled=false
+--       }
+--     },
+--     yaml={
+--       schemas={
+--         ["https://json.schemastore.org/github-workflow.json"]="/.github/workflows/*"
+--       }
+--     }
+--   }
+-- }
 
 -- jsonls
-lsp.jsonls.setup{
-  capabilities=cmp_capabilities,
-}
+-- vim.lsp.jsonls.setup{
+--   capabilities=cmp_capabilities,
+-- }
 
 -- terraform-ls
-lsp.terraformls.setup{
-  capabilities=cmp_capabilities,
-}
+-- vim.lsp.terraformls.setup{
+--   capabilities=cmp_capabilities,
+-- }
 
 -- elixirls
-lsp.elixirls.setup{
-  capabilities=cmp_capabilities,
-  cmd = { "elixir-ls" },
-  elixirLS = {
-    dialyzerEnabled = false,
-    fetchDeps = false
-  },
-  on_attach = require("lsp-format").on_attach,
-}
+-- vim.lsp.elixirls.setup{
+--   capabilities=cmp_capabilities,
+--   cmd = { "elixir-ls" },
+--   elixirLS = {
+--     dialyzerEnabled = false,
+--     fetchDeps = false
+--   },
+--   on_attach = require("lsp-format").on_attach,
+-- }
 
 -- tailwindcss
-lsp.tailwindcss.setup({
-  capabilities=cmp_capabilities,
-  root_dir = lsp.util.root_pattern(
-    "tailwind.config.js",
-    "tailwind.config.js",
-    "package.json",
-    "mix.exs",
-    ".git"
-  ),
-  filetypes = {
-    "html",
-    "elixir",
-    "eelixir",
-    "heex",
-  },
-  init_options = {
-    userLanguages = {
-      elixir = "html-eex",
-      eelixir = "html-eex",
-      heex = "html-eex",
-    }
-  },
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        classRegex = {
-          -- add Elixir classRegex
-          'class[:]\\s*"([^"]*)"',
-        }
-      }
-    }
-  }
-})
+-- vim.lsp.tailwindcss.setup({
+--   capabilities=cmp_capabilities,
+--   root_dir = lsp.util.root_pattern(
+--     "tailwind.config.js",
+--     "tailwind.config.js",
+--     "package.json",
+--     "mix.exs",
+--     ".git"
+--   ),
+--   filetypes = {
+--     "html",
+--     "elixir",
+--     "eelixir",
+--     "heex",
+--   },
+--   init_options = {
+--     userLanguages = {
+--       elixir = "html-eex",
+--       eelixir = "html-eex",
+--       heex = "html-eex",
+--     }
+--   },
+--   settings = {
+--     tailwindCSS = {
+--       experimental = {
+--         classRegex = {
+--           -- add Elixir classRegex
+--           'class[:]\\s*"([^"]*)"',
+--         }
+--       }
+--     }
+--   }
+-- })
 
 -- html
-lsp.html.setup({
-  capabilities=cmp_capabilities,
-  filetypes = { "html", "heex" }
-})
+-- lsp.html.setup({
+--   capabilities=cmp_capabilities,
+--   filetypes = { "html", "heex" }
+-- })
 
